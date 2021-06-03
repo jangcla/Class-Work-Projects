db8940797e332c36b7fc97d5da70aa77af9dce34
trustinyoon <trustinyoon@gmail.com>
2021-06-02 18:29:15 -0700

    started slideable

--- W4D3/Pieces/Slideable.rb
+++ W4D3/Pieces/Slideable.rb
@@ -1,4 +1,6 @@
 module Slideable
+    HORIZONTAL_DIRS = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    DIAGONAL_DIRS = [[-1, 1], [1, 1], [-1, -1], [1, -1]]
 
     def move_dirs
         
@@ -6,6 +8,14 @@ module Slideable
     
 
     def horizontal_dirs
-        
+        HORIZONTAL_DIRS
+    end
+
+    def diagonal_dirs
+      DIAGONAL_DIRS
+    end
+
+    def grow_unblocked_moves_in_dir(dx, dy)
+      
     end
 end
