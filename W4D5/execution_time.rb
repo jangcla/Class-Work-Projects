903061899dac15301e035112cb6db45287a60b24
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-06-04 15:07:21 -0700

    Finished Project

--- W4D5/execution_time.rb
+++ W4D5/execution_time.rb
@@ -27,10 +27,45 @@
 # #time complexity = O(n)
 
 
+# def sub_sum(arr)
+#     sum_arr = []
+#     arr.each.with_index do |el, i|
+#         sum_arr << [el]
+#         arr.each.with_index do |el2, i2|
+#             if i < i2
+#                 sum_arr << (sum_arr[-1].dup << el2)
+#             end
+#         end
+#     end
+#     max = 0
+#     sum_arr.each do |sub|
+#         if sub.sum > max
+#             max = sub.sum
+#         end
+#     end
+#     max
+# end
+
+# time complexity = O(n ^ 2)
+
+
+
 def sub_sum(arr)
-    sum_arr = []
-
-    
-
-
+    max = arr[0]
+    current = arr[0]
+    (1...arr.length).each do |i|
+        if current < 0
+            current = 0 
+        end
+        current += arr[i]
+        if current > max
+            max = current
+        end
+    end
+    max
 end
+
+# Time complexity O(n)...hopefully
+
+list = [2, 3, -6, 7, -6, 7]
+p sub_sum(list)
