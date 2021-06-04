207e62457fb4995dd7a6e947a371c9465de09d4f
jangcla <jangcla@ymail.com>
2021-06-04 12:21:45 -0700

    Finished my_min

---
+++ W4D5/execution_time.rb
@@ -0,0 +1,36 @@
+#Phase 1
+
+# def my_min(arr)
+#     arr.each do |n|
+#         return n if arr.none? {|i| n > i}
+#     end
+# end
+
+# p my_min([ 0, 3, 5, 4, -5, 10, 1, 90 ])
+
+#time complexity = O(n ^ 2)
+
+
+#Phase 2 
+
+# def my_min(arr)
+#     min = arr.first 
+#     (1...arr.length).each do |i|
+#         if arr[i] < min 
+#             min = arr[i]
+#         end
+#     end
+#     min
+# end
+# p my_min([ 0, 3, 5, 4, -5, 10, 1, 90 ])
+
+# #time complexity = O(n)
+
+
+def sub_sum(arr)
+    sum_arr = []
+
+    
+
+
+end
