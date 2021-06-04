a8cce2bc7c7b1a47646aa62ac858a3c09909c924
jangcla <jangcla@ymail.com>
2021-06-04 16:53:54 -0700

    Finished second anagram method

--- W4D5/anagrams_proj/ana_exec_time.rb
+++ W4D5/anagrams_proj/ana_exec_time.rb
@@ -1,3 +1,43 @@
+require "byebug"
 #Phase 1
 
+# def first_anagram(word1, word2)
+#     arr = word1.split("").permutation.to_a
+#     p arr.length
+#     arr.each do |sub_arr|
+#         return true if word2 == sub_arr.join
+#     end
+
+#     false
+
+
+# end
+
+# p  first_anagram("craps", "car")
+
+#This is O(n!)
+
+
+#Phase 2
+
+def second_anagram?(word1, word2)
+  return true if (word1.empty?) && (word2.empty?)
+  return false if ((word1.empty?) && (!word2.empty?)) || ((!word1.empty?) && (word2.empty?)) 
+  letter_1 = word1[0]
+  letter_1_idx = word2.index(letter_1)    
+
+  return false if (letter_1_idx.nil?)
+    word1 = word1.delete(letter_1)
+    word2 = word2.delete(word2[letter_1_idx])
+second_anagram?(word1, word2)
+
+end
+
+p  second_anagram?("tarc", "cart")
+
+# This should be O(n log n)
+
+
+
+  
 
--- W4D5/anagrams_proj/ana_structions.rb
+++ W4D5/anagrams_proj/ana_structions.rb
@@ -11,7 +11,8 @@ Be able to determine the time and space complexity of a method
 Be able to recognize when and how time or space complexity can be improved
 Be able to compare different methods and discuss how changing inputs affects each ones overall runtime
 Phase I:
-Write a method "#first_anagram?" that will generate and store all the possible anagrams of the first string. 
+Write a method "#first_anagram?" that will generate and store all the possible anagrams of 
+the first string. 
 Check if the second string is one of these.
 
 Hints:
@@ -22,10 +23,13 @@ What is the time complexity of this solution? What happens if you increase the s
 
 Phase II:
 Write a method "#second_anagram?" that iterates over the first string. 
-For each letter in the first string, find the index of that letter in the second string (hint: use "Array#find_index") and delete at that index. 
-The two strings are anagrams if an index is found for every letter and the second string is empty at the end of the iteration.
+For each letter in the first string, find the index of that letter in the second string 
+(hint: use "Array#find_index") and delete at that index. 
+The two strings are anagrams if an index is found for every letter and the second string is empty 
+at the end of the iteration.
 
-Try varying the length of the input strings. What are the differences between "#first_anagram?" and "#second_anagram?"?
+Try varying the length of the input strings. What are the differences between "#first_anagram?" 
+and "#second_anagram?"?
 
 Phase III:
 Write a method "#third_anagram?" that solves the problem by sorting both strings alphabetically. 
@@ -43,3 +47,19 @@ What is the time complexity?
 Bonus: Do it with only one hash.
 
 Discuss the time complexity of your solutions together, then call over your TA to look at them.
+
+
+
+Extras
+
+        
+    # while !word1.empty? && !word2.empty?   
+    #     # debugger 
+    #     letter_1 = word1[0]
+    #     letter_1_idx = word2.index(letter_1)   
+
+    #     return false if (letter_1_idx.nil?)
+    #     word1 = word1.delete(letter_1)
+    #     word2 = word2.delete(word2[letter_1_idx])
+    # end
+    #     word1.empty? && word2.empty?
