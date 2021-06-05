15e6b4ad042adb565823b7011334eee06b0e161e
jangcla <jangcla@ymail.com>
2021-06-04 17:01:23 -0700

    Final of the day

--- W4D5/exec_proj/execution_time.rb
+++ W4D5/exec_proj/execution_time.rb
@@ -1,52 +1,52 @@
-#Phase 1
+Phase 1
 
-# def my_min(arr)
-#     arr.each do |n|
-#         return n if arr.none? {|i| n > i}
-#     end
-# end
+def my_min(arr)
+    arr.each do |n|
+        return n if arr.none? {|i| n > i}
+    end
+end
 
-# p my_min([ 0, 3, 5, 4, -5, 10, 1, 90 ])
+p my_min([ 0, 3, 5, 4, -5, 10, 1, 90 ])
 
 #time complexity = O(n ^ 2)
 
 
-#Phase 2 
+Phase 2 
 
-# def my_min(arr)
-#     min = arr.first 
-#     (1...arr.length).each do |i|
-#         if arr[i] < min 
-#             min = arr[i]
-#         end
-#     end
-#     min
-# end
-# p my_min([ 0, 3, 5, 4, -5, 10, 1, 90 ])
+def my_min(arr)
+    min = arr.first 
+    (1...arr.length).each do |i|
+        if arr[i] < min 
+            min = arr[i]
+        end
+    end
+    min
+end
+p my_min([ 0, 3, 5, 4, -5, 10, 1, 90 ])
 
-# #time complexity = O(n)
+#time complexity = O(n)
 
 
-# def sub_sum(arr)
-#     sum_arr = []
-#     arr.each.with_index do |el, i|
-#         sum_arr << [el]
-#         arr.each.with_index do |el2, i2|
-#             if i < i2
-#                 sum_arr << (sum_arr[-1].dup << el2)
-#             end
-#         end
-#     end
-#     max = 0
-#     sum_arr.each do |sub|
-#         if sub.sum > max
-#             max = sub.sum
-#         end
-#     end
-#     max
-# end
+def sub_sum(arr)
+    sum_arr = []
+    arr.each.with_index do |el, i|
+        sum_arr << [el]
+        arr.each.with_index do |el2, i2|
+            if i < i2
+                sum_arr << (sum_arr[-1].dup << el2)
+            end
+        end
+    end
+    max = 0
+    sum_arr.each do |sub|
+        if sub.sum > max
+            max = sub.sum
+        end
+    end
+    max
+end
 
-# time complexity = O(n ^ 2)
+#time complexity = O(n ^ 2)
 
 
 
@@ -65,7 +65,7 @@ def sub_sum(arr)
     max
 end
 
-# Time complexity O(n)...hopefully
+# Time complexity = O(n)...hopefully
 
 list = [2, 3, -6, 7, -6, 7]
 p sub_sum(list)
