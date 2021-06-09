c667456aecda31d30a6c284f8f8c8cabc326308d
Samuel Song <samsongs@umich.edu>
2021-06-09 12:35:38 -0600

    Working on replies table

--- W5D3/import_db.sql
+++ W5D3/import_db.sql
@@ -7,19 +7,22 @@ CREATE TABLE users (
 CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
-    body TEXT NOT NULL
+    body TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
+
+    FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
 CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
-    users_id INTEGER NOT NULL,
+    follower_id INTEGER NOT NULL,
     questions_id INTEGER NOT NULL,
 
-    FOREIGN KEY (users_id) REFERENCES users(id),
+    FOREIGN KEY (follower_id) REFERENCES users(id),
     FOREIGN KEY (questions_id) REFERENCES questions(id)
 );
 
-INSERT INTO 
+INSERT INTO
     users(fname, lname)
 VALUES
     ('Sam', 'Song'),
@@ -27,14 +30,21 @@ VALUES
 
 
 INSERT INTO
-    questions(title, body)
+    questions(title, body, author_id)
 VALUES
-    ('Birthplace', 'Where were you born?'),
-    ('Age', 'How old are you')
+    ('Birthplace', 'Where were you born?', (SELECT id FROM users WHERE fname = 'Sam')),
+    ('Age', 'How old are you', (SELECT id FROM users WHERE fname = 'John'))
 
-    
+-------------------------------------------------------------------------------------------
 
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
 
+    reply VARCHAR(230) NOT NULL,
+    question_id
+);
 
-
-
+INSERT INTO
+    replies(parent_question, reply, reply_id)
+VALUES
+    (1, "asdfjkldaf", 123)
