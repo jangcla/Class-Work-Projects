2419ac997d36080f3e25875e1dad06c7e1b80218
jangcla <jangcla@ymail.com>
2021-06-17 18:54:59 -0700

    Finished all phases of Day 4 project, only bonus questions left

---
+++ Week6/W6D4/FirstRoutes/app/controllers/comments_controller.rb
@@ -0,0 +1,35 @@
+class CommentsController < ApplicationController
+  def index
+    if params.has_key?(:user_id)
+      @user = User.find(params[:user_id])
+      @comments = @user.comments
+    elsif params.has_key?(:artwork_id)
+      @artwork = Artwork.find(params[:artwork_id])
+      @comments = @artwork.comments
+    else
+      @comments = Comment.all
+    end
+    render json: @comments
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save!
+      render json: @comment
+    else
+      render json: @comment.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    render json: @comment
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :artwork_id, :body)
+  end
+end
