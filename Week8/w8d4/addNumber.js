2cdbbd472c0b2af97f2e55cc7b212dc7e6206fba
j3nhan <j3nnynhan@gmail.com>
2021-07-01 15:10:06 -0700

    add numbers file

---
+++ Week8/w8d4/addNumber.js
@@ -0,0 +1,29 @@
+const readline = require('readline');
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+}); 
+
+function addNumbers(sum, numsLeft, completionCallback) {
+    if (numsLeft > 0) {
+        reader.question("Pick a number ", stringNum => {
+            let ourNum = parseInt(stringNum);
+            sum += ourNum;
+            console.log(`Total Sum: ${sum}`);
+
+            addNumbers(sum, numsLeft - 1, completionCallback);
+        });
+    } else {
+        completionCallback(sum);
+    }
+}
+
+addNumbers(0, 3, sum => {
+    console.log(`Total Sum: ${sum}`);
+    reader.close();
+});
+
+// addNumbers(0, 3, function (sum) {
+//   console.log(`Total: ${sum}`);
+//   reader.close();
+// });
