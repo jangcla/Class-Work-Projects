46ccb7e0e6f5d5686330694ae012792108dfd315
lbrilli <lbrilli11@gmail.com>
2021-07-07 15:36:50 -0600

    some css and finish view

--- Week9/W9D3/skeleton/src/ttt-view.js
+++ Week9/W9D3/skeleton/src/ttt-view.js
@@ -2,8 +2,6 @@ class View {
   constructor(game, mark) {
     this.game = game,
     this.mark = mark,
-
-
     this.setupBoard()
   }
 
@@ -13,7 +11,6 @@ class View {
     for (let col = 0; col < 3; col++) {
       for (let row = 0; row < 3; row++) {
         const box = document.createElement("li");
-
         box.dataset.pos = JSON.stringify([col, row]),
         ul.append(box)
       }
@@ -22,11 +19,39 @@ class View {
     
   }
   
-  bindEvents() {}
+  bindEvents() {
+    this.mark.addEventListener("click", this.handleClick)
+  }
 
-  handleClick(e) {}
+  handleClick(e) {
+    const ck = e.target
+    if (ck.nodeName === "LI") {
+      this.makeMove(ck);
+    }
+  }
 
-  makeMove(square) {}
+  makeMove(square) {
+    const pos = JSON.parse(square.dataset.pos)
+    const cPlayer = this.game.currentPlayer;
+
+    if (this.game.board.isEmptyPosition(pos)) {
+      this.game.playMove(pos);
+    } else {
+      alert("This is not a valid move")
+    }
+    square.classList.add(cPlayer)
+    if (this.game.isOver()) {
+      this.handleGameOver()
+    }
+  }
+
+  handleGameOver() {
+    this.mark.removeEventListener("click", this.handleClick)
+    this.mark.classList.add("game-over");
+    const winner = this.game.winner()
+    ele = document.createElement("figcaption");
+    winner ? (this.mark.classList.add(`winner-${winner}`), ele.append(`You win, ${winner}!`)) : ele.append("It's a draw!"), this.mark.append(ele)
+  }
 
 }
 
