431be1d3cf105ba29e393a6a0b3ffcec75d75d8d
jangcla <jangcla@ymail.com>
2021-09-03 12:11:45 -0700

    pushing up progress on old project

--- Week11/W11D2/todo/frontend/todo_redux.jsx
+++ Week11/W11D2/todo/frontend/todo_redux.jsx
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     window.receiveStep = receiveStep;
     window.receiveSteps = receiveSteps;
     window.removeStep = removeStep;
+    
 });
 
 console.log('hello');
