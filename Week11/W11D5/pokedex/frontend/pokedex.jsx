e17956e7988f9ff23ede5828c6162f9485751b46
jangcla <jangcla@ymail.com>
2021-09-14 12:11:45 -0700

    progress on old project

--- Week11/W11D5/pokedex/frontend/pokedex.jsx
+++ Week11/W11D5/pokedex/frontend/pokedex.jsx
@@ -4,4 +4,5 @@ import  ReactDOM  from "react-dom";
 document.addEventListener('DOMContentLoaded', ()=>{
     const root = document.getElementById('root')
     ReactDOM.render(<h1>Pokedex</h1>, root)
+    
 })
