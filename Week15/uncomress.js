b5fe3ab195714951cb55bbd0295444bebbdd3906
jangcla <jangcla@ymail.com>
2021-12-16 12:11:45 -0800

    uncompress DSA files for javascript full attampt

---
+++ Week15/uncomress.js
@@ -0,0 +1,26 @@
+const uncompress = (s) => {
+    let ans = [];
+    let count = '';
+
+    for (let i = 0; i < s.length; i++) {
+
+        if (parseInt(s[i]) >= 0) {
+            count += s[i];
+        } else {
+            for (let j = 0; j < parseInt(count); j++) {
+                ans.push(s[i]);
+            }
+            count = '';
+        }
+
+    }
+
+    return ans.join("");
+};
+
+
+// n = number of groups
+// m = max num found in any group
+// Time: O(n*m) as the potential is multiplied by the max amount
+// Space: O(n*m) as the potential is multiplied by the max amount
+
